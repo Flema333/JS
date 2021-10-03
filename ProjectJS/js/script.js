a22374ba01c914a9f080d94c38d3b6e507e4b7fa
rost <Rost@djopa.com>
2021-10-03 23:25:34 +0300

    first commit

---
+++ ProjectJS/js/script.js
@@ -0,0 +1,49 @@
+"use strict";
+
+/* alert('Hello'); */  //повідомлення на сайті
+
+/* const result = confirm('Do you pidar');  //підтвердження на сайті
+console.log(result); */
+
+/* const answer = +prompt('Do you pidar', '18'); /* //сткрока де можна вписувати з відповідю "18" - значення
+
+
+/* console.log(answer + 5); */
+
+/* const answer = []; */
+
+/* answer[0] = prompt('Do you pidar', '');
+answer[1] = prompt('real pidar', '');
+answer[2] = prompt('fuck you', ''); */
+
+/* console.log(typeof(answer)); */
+
+
+/* const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`); */
+
+/* const user = 'Niger'; */
+
+/* const user = prompt('Say me you stupid name', '');
+alert(`Hi my dear, ${user}`); */
+
+
+/* console.log('arr' + ' - object');
+
+let incr = 10,
+    decr = 10;
+
+/* incr++; //incriment
+decr--; //decriment */
+
+/* console.log(incr, decr);
+
+console.log(5%2);
+
+console.log(2*4 === '8'); */
+
+/* const isChecked = false,
+    isClose = false;
+
+console.log(isChecked || isClose); */
